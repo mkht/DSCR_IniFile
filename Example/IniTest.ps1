5eb70fd2a2492e3b6f7dffc3559ee05d202ed240
mkht <mkht@users.noreply.github.com>
2018-07-03 11:40:33 +0900

    Add sample for Absent

--- Example/IniTest.ps1
+++ Example/IniTest.ps1
@@ -7,10 +7,19 @@ Configuration IniTest
     {
         cIniFile IniTest
         {
-            Ensure = "Present"
+            Ensure = "Present"  #create key
             Path = "C:\IniTest.ini"
             Key = 'TestKey'
             Value = 'TestValue'
+            Section = ''    #no section
+            Encoding = 'UTF8'
+        }
+
+        cIniFile IniTest2
+        {
+            Ensure = "Absent"   #remove key
+            Path = "C:\IniTest.ini"
+            Key = 'MissingKey'
             Section = 'Section'
             Encoding = 'UTF8'
         }  
